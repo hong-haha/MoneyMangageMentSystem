a3a445954ac9c3a117ea3db1ea0f8b9ee8ab600e
hong-haha <hamh3118@gmail.com>
2021-03-09 17:55:55 +0900

    removed Hello.java and added moneymanager.gava

---
+++ src/MoneyManager.java
@@ -0,0 +1,13 @@
+
+public class MoneyManager {
+
+	public static void main(String[] args) {
+		System.out.println("1. Add Money");
+		System.out.println("2. Delete Money");
+		System.out.println("3. Edit Money");
+		System.out.println("4. View Money");
+		System.out.println("5. Exit Money");
+
+	}
+
+}
