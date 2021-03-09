512b0787adb9924fd3dbab1e82b0b629728ececf
hong-haha <hamh3118@gmail.com>
2021-03-09 23:24:48 +0900

    메뉴 수정

--- src/MoneyManager.java
+++ src/MoneyManager.java
@@ -2,11 +2,11 @@
 public class MoneyManager {
 
 	public static void main(String[] args) {
-		System.out.println("1. Add Money");
-		System.out.println("2. Delete Money");
-		System.out.println("3. Edit Money");
-		System.out.println("4. View Money");
-		System.out.println("5. Exit Money");
+		System.out.println("1. 수입입력(Income)");
+		System.out.println("2. 지출입력(Expenditure)");
+		System.out.println("3. 이체입력(Transfer)");
+		System.out.println("4. 전체내용보기(View all)");
+		System.out.println("5. Exit ");
 
 	}
 
